4efbe32226458fc00393988835b4866c016aab15
Rachael Mahon <rachmahon@gmail.com>
2016-09-15 21:51:53 +0100

    Bloc added

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
 
-puts "The students of Villains Academy"
-puts "_________________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Withc of the West"
-puts "The Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally, we print the total number of students
-print "Overall, we have #{student_count} great students"
+students = [
+
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "The Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+ ]
+# and then prints them
+students.each do |student|
+  puts student
+end
+
+# finally, we print the total
+print "Overall, we have #{students.count} great students"
