76d10e1e3ef5224294eec3ad29d12a4e9833a474
Rachael Mahon <rachmahon@gmail.com>
2016-09-15 23:37:39 +0100

    8.1 Added index to list

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-
-def print_header
-    puts "The students of Villains Academy"
-    puts "______________"
-end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer(students)
-  puts "Overall we have #{students.count} great students"
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -28,7 +12,27 @@ def input_students
   students
 end
 
+
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "______________"
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall we have #{students.count} great students"
+end
+
+
+
 students = input_students
 print_header
 print(students)
 print_footer(students)
+input_students
