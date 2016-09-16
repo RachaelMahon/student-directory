155bf1504c3006aeedbda3ff7c9e092305d7b0a4
Rachael Mahon <rachmahon@gmail.com>
2016-09-16 16:36:24 +0100

    Added more details about the student - deleted two methods for prointing footer and printing and incorpoated them into the bottom of the input method

--- directory.rb
+++ directory.rb
@@ -4,7 +4,8 @@ def input_students
   puts "To finish, hit return twice"
   puts "Student name?:"
   name = gets.chomp
-  unless !name.empty? do
+  puts "Cohort: "
+  cohort = gets.chomp
   puts "Hobbies?: "
   hobbies = gets.chomp
   puts "Country of birth?:"
@@ -14,39 +15,35 @@ def input_students
 
 
   students = []
-  students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country_of_birth, height: height}
-  end
-end
-end
-
-
-
-
-def print_header
-    puts "The students of Villains Academy"
-    puts "______________"
-end
-
-def print (students)
-  index = 0
-  while index < students.length
-    puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
-    index += 1
+  while !name.empty? do
+  students << {name: name, cohort: cohort, hobbies: hobbies, country_of_birth: country_of_birth, height: height}
+  if students.to_a.count == 1
+      puts "You have entered #{students.count} student!"
+    elsif students.count > 1
+      puts "You have entered #{students.count} students!"
     end
+  puts "Please add more or press return twice to finish"
+  puts "Name: "
+  name = gets.chomp
+  if !name.empty?
+    puts "Cohort: "
+    cohort = gets.chomp
+    puts "Hobbies: "
+    hobbies = gets.chomp
+    puts "Country of birth?: "
+    country_of_birth = gets.chomp
+    puts "Height: "
+    height = gets.chomp
+  end
+    end
+
+
+  idx = 0
+  while idx < students.to_a.length
+    puts "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort). Hobbies include #{students[idx][:hobbies]}. Born in #{students [idx] [:country_of_birth]}. Height = #{students [idx][:height]}"
+    idx += 1
+
+end
 end
 
-
-
-def print_footer(students)
-  if students.count = 1
-    puts "Overall we have #{students.count} great student"
-  else
-  puts "Overall we have #{students.count} great students"
-end
-end
-
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+input_students
