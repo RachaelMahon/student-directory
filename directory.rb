f617b7337ea036eb0ceb2b637d9e7dfc498b2882
Rachael Mahon <rachmahon@gmail.com>
2016-09-18 18:05:56 +0100

    Interactive menu added

--- directory.rb
+++ directory.rb
@@ -44,34 +44,34 @@ def input_students
     height = gets.chomp
     height.empty? ? height = "Unknown" : height
   else "Must input at least one student"
-  end
+end
+end
+end
 
 
     #students = students.sort_by {|k| k['cohort']}
 
-    puts "What cohort would you like to print? If all press enter"
+    #puts "What cohort would you like to print? If all press enter"
     # Couldn't get this to work - puts students.inject ([]) {|result, h| result << h unless result.include?(h); result}
-    selection = gets.chomp
+    #selection = gets.chomp
 
     # students.each do |key, value|
     #   if key =
     #  puts value.to_s
     #  end
     #end
-   puts "The students of Unreal Academy".center(50)
-   puts "-------------------------------------------------".center(50)
 
+
+
+def print(students)
   idx = 0
-
-
-
-  #if selection.empty?
     print_selection = []
   while idx < students.to_a.length
     print_selection << "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort). Hobbies include #{students[idx][:hobbies]}. Born in #{students [idx] [:country_of_birth]}. Height = #{students [idx][:height]}"
     idx += 1
+    puts print_selection
   end
-
+end
 
   #if selection.empty?
   #  print_selection = []
@@ -93,12 +93,48 @@ def input_students
   #  end
   #  end
 
-puts print_selection
 
+
+
+def print_header
+  puts "The students of Unreal Academy".center(50)
+  puts "-------------------------------------------------".center(50)
+end
+
+def print_footer(students)
+  puts "Overall we have #{students.count} great students!"
+  end
+
+
+
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+end
 end
 
 
 
-end
-
+interactive_menu
 input_students
