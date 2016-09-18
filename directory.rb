bb039b97d090e92abc2163d193bb938cd75e1d0e
Rachael Mahon <rachmahon@gmail.com>
2016-09-18 14:29:53 +0100

    8.8 Found it very difficult to get all elements of this section working. Going to come back to it

--- directory.rb
+++ directory.rb
@@ -41,15 +41,54 @@ def input_students
   end
     end
 
+    #students = students.sort_by {|k| k['cohort']}
+
+    puts "What cohort would you like to print? If all press enter"
+    # Couldn't get this to work - puts students.inject ([]) {|result, h| result << h unless result.include?(h); result}
+    selection = gets.chomp
+
+    # students.each do |key, value|
+    #   if key =
+    #  puts value.to_s
+    #  end
+    #end
    puts "The students of Unreal Academy".center(50)
    puts "-------------------------------------------------".center(50)
 
   idx = 0
-  while idx < students.to_a.length
-    puts "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort). Hobbies include #{students[idx][:hobbies]}. Born in #{students [idx] [:country_of_birth]}. Height = #{students [idx][:height]}"
-    idx += 1
 
+
+
+  #if selection.empty?
+    print_selection = []
+  while idx < students.to_a.length
+    print_selection << "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort). Hobbies include #{students[idx][:hobbies]}. Born in #{students [idx] [:country_of_birth]}. Height = #{students [idx][:height]}"
+    idx += 1
+  end
+
+  #if selection.empty?
+  #  print_selection = []
+  #  correct_cohort = students.delete_if{ |k, v| v != selection}
+  #  while idx < students.to_a.length
+  #    print_selection << "#{idx+1}. #{correct_cohort[idx][:name]} (#{correct_cohort[idx][:cohort]} cohort). Hobbies include #{correct_cohort[idx][:hobbies]}. Born in #{correct_cohort[idx] [:country_of_birth]}. Height = #{correct_cohort[idx][:height]}"
+  #    idx += 1
+  #  end
+#  end
+
+  #else
+  #  while idx < students.to_a.length
+  #    while selection == students[keys[1]
+      #student_selection = []
+      #student_selection = students.select{|k, v| v == selection}.to_h
+      #print_selection << "#{idx+1}. #{student_selection[idx][:name]} (#{student_selection[idx][:cohort]} cohort). Hobbies include #{student_selection[idx][:hobbies]}. Born in #{student_selection [idx] [:country_of_birth]}. Height = #{student_selection [idx][:height]}"
+  #    print_selection << "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort). Hobbies include #{students[idx][:hobbies]}. Born in #{students [idx] [:country_of_birth]}. Height = #{students [idx][:height]}"
+  #    idx += 1
+  #  end
+  #  end
+   puts print_selection
 end
-end
+
+
+
 
 input_students
